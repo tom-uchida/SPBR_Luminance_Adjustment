807b679d0ce8d1f2fe1385190703c8681749d271
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-03-14 00:49:55 +0900

    remove

--- aaa.cc
+++
@@ -1,9 +0,0 @@
-        //----- StandardBoundingBox ----- [OK] //ST_BOUNDING_BOX
-        if ( !strncmp( buf, STANDARD_BOUNDING_BOX_COMMAND, strlen(BOUNDING_BOX_COMMAND) ) ) { 
-          double xmin, ymin, zmin, xmax, ymax, zmax ;
-          sscanf ( buf, "%s %lg %lg %lg %lg %lg %lg", dummy, &xmin, &ymin, &zmin, &xmax, &ymax, &zmax ) ;
-	  kvs::Vector3f minPoint ( xmin, ymin, zmin );
-	  kvs::Vector3f maxPoint ( xmax, ymax, zmax );
-	  setStandardBoundingBox ( minPoint, maxPoint ) ;
-
-        } else 
